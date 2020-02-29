038086bb6393a406af72320d6251f645d481b84f
Dimitris Ganotis <dimitrisgan97@gmail.com>
2020-02-29 15:56:32 +0200

    Laravel installation

--- con_logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-    session_start();
-    unset($_SESSION['username'], $_SESSION['userID'], $_SESSION['status']);
-    session_destroy();
-    header('Location: page_register_login.php?msg=Αποσυνδεθήκατε επιτυχώς!');
-    exit();
-
-?>
